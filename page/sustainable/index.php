a09e45012bb0d3cb56179a8052747db3e505984d
dh_lemanh <Dfd2hiug>
2024-02-17 19:40:16 +0900

    MV PC

--- page/sustainable/index.php
+++ page/sustainable/index.php
@@ -60,6 +60,129 @@ $page_shareurl = 'https://'.STORE_NAME.'.parco.jp/page/sustainable/'; //必ず�
 
 </header>
 <main class="main-contents">
+    <section class="mv">
+        <div class="mv-body">
+            <div class="mv-listitem">
+                <div class="mv-listitem-01">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_main_pc_01.png" alt="">
+                </div>
+                <div class="mv-listitem-02">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_main_pc_02.png" alt="">
+                </div>
+                <div class="mv-listitem-03">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_main_pc_03.png" alt="">
+                </div>
+                <div class="mv-listitem-04">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_main_pc_04.png" alt="">
+                </div>
+                <div class="mv-listitem-05">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_main_pc_05.png" alt="">
+                </div>
+                <div class="mv-listitem-06">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_01.png" alt="">
+                </div>
+                <div class="mv-listitem-07">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_02.png" alt="">
+                </div>
+                <div class="mv-listitem-08">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_03.png" alt="">
+                </div>
+                <div class="mv-listitem-09">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_04.png" alt="">
+                </div>
+                <div class="mv-listitem-10">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_05.png" alt="">
+                </div>
+                <div class="mv-listitem-11">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_06.png" alt="">
+                </div>
+                <div class="mv-listitem-12">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_07.png" alt="">
+                </div>
+                <div class="mv-listitem-13">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_08.png" alt="">
+                </div>
+                <div class="mv-listitem-14">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_09.png" alt="">
+                </div>
+                <div class="mv-listitem-15">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_10.png" alt="">
+                </div>
+                <div class="mv-listitem-16">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_11.png" alt="">
+                </div>
+                <div class="mv-listitem-17">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_12.png" alt="">
+                </div>
+                <div class="mv-listitem-18">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_13.png" alt="">
+                </div>
+                <div class="mv-listitem-19">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_14.png" alt="">
+                </div>
+                <div class="mv-listitem-20">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_15.png" alt="">
+                </div>
+                <div class="mv-listitem-21">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_16.png" alt="">
+                </div>
+                <div class="mv-listitem-22">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_17.png" alt="">
+                </div>
+                <div class="mv-listitem-23">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_18.png" alt="">
+                </div>
+                <div class="mv-listitem-24">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_19.png" alt="">
+                </div>
+                <div class="mv-listitem-25">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_20.png" alt="">
+                </div>
+                <div class="mv-listitem-26">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_21.png" alt="">
+                </div>
+                <div class="mv-listitem-27">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_22.png" alt="">
+                </div>
+                <div class="mv-listitem-28">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_23.png" alt="">
+                </div>
+                <div class="mv-listitem-29">
+                    <img src="/<?php echo DIRNAME; ?>/assets/images/mv/fukidashi_pc_24.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="ribo-text">
+        <div class="container">
+            <div class="ribo-text-english">
+                KICHIJOJI PARCO SUSTAINABLE DAYS
+            </div>
+            <h2>
+                みんなのサステナ宣言展
+            </h2>
+            <div class="ribo-text-info">
+                <p>サステナブルな社会の実現に向け、皆さまから募集した「宣言」を、できるだけたくさん、館内のさまざまな場所で展示する企画です。<br>
+                そのほかにも、ワークショップやポップアップショップなど、さまざまな学びを得られるイベントが盛りだくさん。</p>  
+                <p class="ribo-text-buttom-line">さあ、未来をより良く生きるためのヒントを見つけにいこう。</p>
+                
+            </div>
+            <div class="ribo-text-timeline">
+                <ul class="ribo-text-timeline-list">
+                    <li class="ribo-text-timeline-item">期間　2024年3月15日（金）〜 3月31日（日）</li>
+                    <li class="ribo-text-timeline-item">展示場所　1F イベントスペース　1F・2F各所</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="event">
+        <div class="container">
+            <h2 class="icon-title">
+                <img src="assets/images/event-title.svg" alt="Event">
+            </h2>
+        </div>
+    </section>
 </main>
 
 
